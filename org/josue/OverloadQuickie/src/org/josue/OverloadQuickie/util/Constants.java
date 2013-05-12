6797ca29a6ffe7ac39e9b3f880dc7582c71dc5a5
Josue Magallanes <josue_m_23@hotmail.com>
2013-05-12 02:48:36 -0400

    new
    second script

---
+++ org/josue/OverloadQuickie/src/org/josue/OverloadQuickie/util/Constants.java
@@ -0,0 +1,11 @@
+package org.josue.OverloadQuickie.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Josue
+ * Date: 5/9/13
+ * Time: 1:05 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Constants {
+}
